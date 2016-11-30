6c8c8c6cc15edd62939b67beeac556d4e852f2d3
Ying Wang <yingw787@gmail.com>
2016-11-29 22:37:14 -0500

    Up to and including Lecture 47.

---
+++ index.ios.js
@@ -0,0 +1,20 @@
+// index.ios.js - place code in here for iOS
+
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a component
+const App = () => (
+    <View style={{ flex: 1 }}>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
+);
+
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
